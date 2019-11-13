fb27fc8012a9a2b47bfd94665adc19f2600ca085
Stijn <hanssenstijn@gmail.com>
2019-11-13 19:22:34 +0100

    chapter2
    - simple string statements
    - use the dot (.) extent a function on a variable
    - combine string statement
    - add extra lines to the string

---
+++ chapter2.py
@@ -0,0 +1,23 @@
+message = "Hello Python world!"
+print(message)
+message = "Hello Python Crash Course world!"
+print(message)
+
+# The dot (.) after name in name.title() tells Python to make the title() method
+# act on the variable name. Every method is followed by a set of parentheses,
+# because methods often need additional information to do their work.
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# combining strings
+first_name = "S"
+last_name = "H."
+full_name = first_name + " " + last_name
+print(full_name)
+
+# add new line to string
+print("Languages:\nPython\nJavaScript")
+
+
