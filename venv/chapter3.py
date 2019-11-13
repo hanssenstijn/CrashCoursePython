d7084d4b19fcdb24c34b742d4d02e3e472339b95
Stijn <hanssenstijn@gmail.com>
2019-11-13 19:38:03 +0100

    chapter 3
    -create a list
    -change a value in a list
    - add a value to the list
    - instert a value at a specific position
    - delete a value of the list

---
+++ venv/chapter3.py
@@ -0,0 +1,17 @@
+# develop a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+# acces element of list
+print(bicycles[0])
+# change value
+bicycles[0] = 'Gazelle'
+print(bicycles)
+# append to list
+bicycles.append('Swap')
+print(bicycles)
+# or insert for positin
+bicycles.insert(0,'Swap')
+print(bicycles)
+# remove item
+del bicycles[0]
+print(bicycles)
