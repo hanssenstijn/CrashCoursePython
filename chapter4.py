9c7584e3888d3ba76704facb98b02708d8fd0226
Stijn <hanssenstijn@gmail.com>
2019-11-13 20:54:42 +0100

    chapter 4
    - develop a list and loop over it
    - range function for easy development of numbers
    - set a open list, define it with append in the loop to fill it
    - develop a tulpe
    - coding style no more than 80 characthers in the line

---
+++ chapter4.py
@@ -0,0 +1,24 @@
+# set list, initialize a loop
+magicians = ['alice', 'david', 'carolina']
+for x in magicians:
+    print(x)
+
+# range function for series of numbers
+for value in range(1,5):
+    print(value)
+
+# store for loop into list
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
+
+# define a tuple, using round brackets
+dimensions = (200, 50)
+print(dimensions[1])
+
+# line length
+# Many Python programmers recommend that each line should be less than
+# 80 characters.
+
