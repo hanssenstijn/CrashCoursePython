4516d5c31b759fc1afdb714c1335378337ff4a3b
Stijn <hanssenstijn@gmail.com>
2019-11-14 20:35:21 +0100

    functions in python
    -basic function with no input variable
    - input value for the function

---
+++ functions.py
@@ -0,0 +1,16 @@
+# basic function
+def greetuser():
+    """ Dis[;ay a simple greeting"""
+    print("Hello!")
+
+greetuser()
+
+# input for function
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello, " + username.title() + "!")
+greet_user('jesse')
+
+# import packages
+# file --> settings --> project settings --> project interpeter
+
