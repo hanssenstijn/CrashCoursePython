aeee2df8e0178ddb51804949e5582b8f240c3b2c
Stijn <hanssenstijn@gmail.com>
2019-11-14 20:11:27 +0100

    easy input example and whlile loop
    - comment an easy input function
    - while look ( dont forget to include the tabs)

---
+++ UserInputWhileloops.py
@@ -0,0 +1,9 @@
+# use input
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+# simple while loop
+current_number = 1
+while current_number <= 3:
+    print(current_number)
+    current_number += 1
